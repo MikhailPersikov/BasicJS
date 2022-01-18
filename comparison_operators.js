70de4891d5d0aff598e68dac7f4bd1effd652819
MikhailPersikov <gt.gluck@gmail.com>
2022-01-18 21:35:10 +0200

    Add practice conditional_branching.js

--- comparison_operators.js
+++ comparison_operators.js
@@ -1,9 +1,17 @@
-console.log(5 > 4);//true
-console.log("ананас" > "яблоко"); //false
-console.log("2" > "12"); //true
-console.log(undefined == null); //true
-console.log(undefined === null); //false
-console.log(null == "\n0\n"); //false
-console.log(null === +"\n0\n"); //false
+// console.log(5 > 4);//true
+// console.log("ананас" > "яблоко"); //false
+// console.log("2" > "12"); //true
+// console.log(undefined == null); //true
+// console.log(undefined === null); //false
+// console.log(null == "\n0\n"); //false
+// console.log(null === +"\n0\n"); //false
 
 // console.log('\n 0 \n');
+
+const str = "123";
+console.log(typeof(str));
+Number(str) 
+console.log(typeof(str));
+const y = +str;
+console.log(typeof(y));
+
